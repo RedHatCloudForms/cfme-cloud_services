599eaf182b936a859c1e21060d3af3ebfef758c8
Libor Pichler <lpichler@redhat.com>
2019-07-15 15:06:49 +0200

    Download manifest with schema in ManifestFetcher

--- lib/cfme/cloud_services/manifest_fetcher.rb
+++ lib/cfme/cloud_services/manifest_fetcher.rb
@@ -4,164 +4,30 @@ class Cfme::CloudServices::ManifestFetcher
     block_given? ? yield(manifest) : manifest
   end
 
+  private_class_method def self.certificate_config
+    {:ssl_client_cert => OpenSSL::X509::Certificate.new(File.read("/etc/pki/consumer/cert.pem")),
+     :ssl_client_key  => OpenSSL::PKey::RSA.new(File.read("/etc/pki/consumer/key.pem")),
+     :verify_ssl      => OpenSSL::SSL::VERIFY_PEER}
+  end
+
   private_class_method def self.raw_manifest
-    # TODO: Download the manifest from the cloud for this CF version...for now just hardcode
-    #
     # TODO: Modeling
     #   - Should we allow collection of non-model information, such as replication,
     #     pg_* tables or filesystem level things?
-    common = {
-      "id"                  => nil,
-      "name"                => nil,
-      "type"                => nil,
-      "guid"                => nil,
-      "api_version"         => nil,
-      "emstype_description" => nil,
-      "hostname"            => nil,
-      "vms"                 => {
-        "id"                   => nil,
-        "name"                 => nil,
-        "type"                 => nil,
-        "guid"                 => nil,
-        "uid_ems"              => nil,
-        "archived"             => nil,
-        "cpu_cores_per_socket" => nil,
-        "cpu_total_cores"      => nil,
-        "disks_aligned"        => nil,
-        "ems_ref"              => nil,
-        "has_rdm_disk"         => nil,
-        "host_id"              => nil,
-        "linked_clone"         => nil,
-        "orphaned"             => nil,
-        "power_state"          => nil,
-        "ram_size_in_bytes"    => nil,
-        "retired"              => nil,
-        "v_datastore_path"     => nil,
-        "hardware"             => {
-          "id"    => nil,
-          "disks" => {
-            "id"           => nil,
-            "device_name"  => nil,
-            "device_type"  => nil,
-            "disk_type"    => nil,
-            "free_space"   => nil,
-            "mode"         => nil,
-            "size"         => nil,
-            "size_on_disk" => nil,
-            "partitions"   => {
-              "id"                => nil,
-              "name"              => nil,
-              "controller"        => nil,
-              "free_space"        => nil,
-              "partition_type"    => nil,
-              "size"              => nil,
-              "start_address"     => nil,
-              "used_space"        => nil,
-              "virtual_disk_file" => nil,
-              "volume_group"      => nil,
-              "volumes"           => {
-                "id"           => nil,
-                "name"         => nil,
-                "filesystem"   => nil,
-                "free_space"   => nil,
-                "size"         => nil,
-                "typ"          => nil,
-                "used_space"   => nil,
-                "volume_group" => nil,
-              }
-            }
-          }
-        }
-      }
-    }
 
-    infra_common = {
-      "ems_clusters" => {
-        "id"               => nil,
-        "uid_ems"          => nil,
-        "ems_ref"          => nil,
-        "ha_enabled"       => nil,
-        "drs_enabled"      => nil,
-        "effective_cpu"    => nil,
-        "effective_memory" => nil,
-      },
-      "hosts"        => {
-        "id"                   => nil,
-        "name"                 => nil,
-        "type"                 => nil,
-        "hostname"             => nil,
-        "ipaddress"            => nil,
-        "power_state"          => nil,
-        "guid"                 => nil,
-        "uid_ems"              => nil,
-        "mac_address"          => nil,
-        "maintenance"          => nil,
-        "vmm_vendor"           => nil,
-        "vmm_version"          => nil,
-        "vmm_product"          => nil,
-        "vmm_buildnumber"      => nil,
-        "archived"             => nil,
-        "cpu_cores_per_socket" => nil,
-        "cpu_total_cores"      => nil,
-        "ems_cluster_id"       => nil,
-      },
-      "storages" => {
-        "id"                  => nil,
-        "name"                => nil,
-        "location"            => nil,
-        "store_type"          => nil,
-        "total_space"         => nil,
-        "free_space"          => nil,
-        "uncommitted"         => nil,
-        "storage_domain_type" => nil,
-        "host_storages"       => {
-          "ems_ref" => nil,
-          "host_id" => nil,
-        }
-      }
-    }
+    manifest_config = ::Settings.cfme_cloud_services.manifest_configuration
 
-    {
-      "cfme_version" => Vmdb::Appliance.VERSION,
-      "manifest" => {
-        "core" => {
-          "MiqDatabase" => {
-            "id"                 => nil,
-            "guid"               => nil,
-            "region_number"      => nil,
-            "region_description" => nil,
-          },
-          "Zone" => {
-            "id"   => nil,
-            "name" => nil
-          }
-        },
-        "ManageIQ::Providers::OpenStack::CloudManager" => common.clone,
-        "ManageIQ::Providers::Redhat::InfraManager"    => common.clone.merge(
-          infra_common
-        ),
-        "ManageIQ::Providers::Vmware::InfraManager"    => common.clone.merge(
-          "ems_extensions" => {
-            "id"      => nil,
-            "ems_ref" => nil,
-            "key"     => nil,
-            "company" => nil,
-            "label"   => nil,
-            "summary" => nil,
-            "version" => nil
-          },
-          "ems_licenses"   => {
-            "id"              => nil,
-            "ems_ref"         => nil,
-            "name"            => nil,
-            "license_edition" => nil,
-            "total_licenses"  => nil,
-            "used_licenses"   => nil,
-          },
-        ).merge(
-          infra_common
-        ),
-      }
-    }.to_json
+    uri = URI::Generic.build(
+      :scheme => manifest_config.scheme,
+      :host   => manifest_config.host,
+      :port   => manifest_config.port,
+      :path   => File.join(manifest_config.path, Vmdb::Appliance.VERSION)
+    )
+
+    response = RestClient::Resource.new(uri.to_s, certificate_config)
+    response.get
+  rescue StandardError => e
+    _log.error("Error with obtaining manifest with schema: #{e.message}")
+    JSON.generate({})
   end
 end
---
+++ spec/manifest_fetcher_spec.rb
@@ -0,0 +1,68 @@
+RSpec.describe Cfme::CloudServices::ManifestFetcher do
+  describe "#fetch" do
+    let(:cfme_version) { Vmdb::Appliance.VERSION }
+
+    let(:expected_json_manifest) do
+      {"cfme_version" => cfme_version,
+       "manifest"     => {"ManageIQ::Providers::OpenStack::CloudManager" => {
+           "id"   => nil,
+           "name" => nil
+       }}}
+    end
+
+    let(:manifest_response) do
+      <<~EOJ
+        {
+          "cfme_version":"#{cfme_version}",
+          "manifest":
+          {
+            "ManageIQ::Providers::OpenStack::CloudManager":{"id":null,"name":null}
+          }
+        }
+      EOJ
+    end
+
+    let(:key) do
+      OpenSSL::PKey::RSA.new(2048)
+    end
+
+    let(:cert) do
+      name = OpenSSL::X509::Name.parse("CN=example.com/C=EE")
+      cert = OpenSSL::X509::Certificate.new
+      cert.version     = 2
+      cert.serial      = 0
+      cert.not_before  = Time.now
+      cert.not_after   = cert.not_before + 1 * 365 * 24 * 60 * 60 # 1 year validity
+      cert.public_key  = key.public_key
+      cert.subject     = name
+      cert.issuer      = name
+      cert.sign key, OpenSSL::Digest::SHA1.new
+      cert
+    end
+
+    let(:certificate) do
+      {:ssl_client_cert => cert.to_pem,
+       :ssl_client_key  => key,
+       :verify_ssl      => OpenSSL::SSL::VERIFY_PEER}
+    end
+
+    it "fetches manifest" do
+      uri_config = ::Settings.cfme_cloud_services.manifest_configuration
+      uri = URI::Generic.build(
+        :scheme => uri_config.scheme,
+        :host   => uri_config.host,
+        :port   => uri_config.port,
+        :path   => File.join(uri_config.path, Vmdb::Appliance.VERSION)
+      )
+
+      allow(described_class).to receive(:certificate_config).and_return(certificate)
+
+      require 'rest-client'
+
+      expect(RestClient::Resource).to receive(:new).with(uri.to_s, certificate).and_return(RestClient::Resource.new(nil))
+      allow_any_instance_of(RestClient::Resource).to receive(:get).and_return(manifest_response)
+
+      expect(described_class.send(:fetch)).to eq(expected_json_manifest)
+    end
+  end
+end
