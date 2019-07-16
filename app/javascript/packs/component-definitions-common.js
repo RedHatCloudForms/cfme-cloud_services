bbd1e2a8f136facf53ad0dbff0381fbac839fd7c
PanSpagetka <rknaur@redhat.com>
2019-07-16 10:20:59 +0200

    Add Red Hat logo as menu icon

--- app/javascript/packs/component-definitions-common.js
+++ app/javascript/packs/component-definitions-common.js
@@ -1,4 +1,5 @@
-import RedHatCloudServicesList from './red_hat_cloud_services_list'
-import RedHatCloudServicesRedirect from './red_hat_cloud_services_redirect'
+import RedHatCloudServicesList from './red_hat_cloud_services_list';
+import RedHatCloudServicesRedirect from './red_hat_cloud_services_redirect';
+import '../../assets/stylesheets/cfme-cloud_services.scss';
 ManageIQ.component.addReact('RedHatCloudServicesList', RedHatCloudServicesList);
 ManageIQ.component.addReact('RedHatCloudServicesRedirect', RedHatCloudServicesRedirect);
