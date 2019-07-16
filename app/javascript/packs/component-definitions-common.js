9abd8f859f56c2d8dc3d9b26e9ffec872f35d7c5
PanSpagetka <rknaur@redhat.com>
2019-07-16 16:31:01 +0200

    Make menu logo grey

--- app/javascript/packs/component-definitions-common.js
+++ app/javascript/packs/component-definitions-common.js
@@ -1,5 +1,4 @@
 import RedHatCloudServicesList from './red_hat_cloud_services_list';
 import RedHatCloudServicesRedirect from './red_hat_cloud_services_redirect';
-import '../../assets/stylesheets/cfme-cloud_services.scss';
 ManageIQ.component.addReact('RedHatCloudServicesList', RedHatCloudServicesList);
 ManageIQ.component.addReact('RedHatCloudServicesRedirect', RedHatCloudServicesRedirect);
