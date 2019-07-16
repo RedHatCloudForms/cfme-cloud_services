18a7a271588640cc3d652a317fb3b0dd1050c364
Alberto Bellotti <abellott@redhat.com>
2019-07-15 23:21:10 -0400

    Adding the sync action returning the action result signature which includes the task id of async actions.

--- app/controllers/api/redhat_cloud_service_providers_controller.rb
+++ app/controllers/api/redhat_cloud_service_providers_controller.rb
@@ -9,17 +9,29 @@ module Api
       klass = collection_class(collection_type)
       provider = find_resource(klass, "id", id)
       provider = filter_resource(provider, collection_type, klass)
-      raise NotFoundError, "Invalid RedHat Cloud Service Provider id #{id} specified" unless provider_types.include?(provider.type)
+      raise NotFoundError, "Invalid #{provider_what} id:#{id} specified" unless provider_types.include?(provider.type)
 
       provider
     end
 
-    def sync_resource(type, id, data)
+    def sync_resource(_type, id, _data)
       provider = find_redhat_cloud_service_providers(id)
-      $log.info "XXXXXX - #{type}, provider type = #{provider.type} id = #{provider.id}, #{data}"
+      desc = "Syncing #{provider_ident(provider)}"
+      task_id = Cfme::CloudServices::InventorySync.sync_queue(User.current_user.userid, provider)
+      action_result(true, desc, :task_id => task_id)
+    rescue => e
+      action_result(false, e.to_s)
     end
 
     private
+
+    def provider_ident(provider)
+      "#{provider_what} id:#{provider.id} name:'#{provider.name}'"
+    end
+
+    def provider_what
+      "RedHat Cloud Service Provider"
+    end
 
     def provider_types
       Cfme::CloudServices::ManifestFetcher.fetch["manifest"].keys.reject { |k| k == "core" }.uniq
