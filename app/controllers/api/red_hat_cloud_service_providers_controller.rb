09dced96c74ea3243f7ad9a055b9590d4e7f6fcc
Alberto Bellotti <abellott@redhat.com>
2019-07-22 15:37:34 -0400

    Adding support for API Collection actions
    POST /api/red_hat_cloud_service_providers   - action "sync"       with provider_ids specified.
    POST /api/red_hat_cloud_service_providers   - action "sync_all"
    POST /api/red_hat_cloud_services            - action "sync_platform"

--- app/controllers/api/red_hat_cloud_service_providers_controller.rb
+++ app/controllers/api/red_hat_cloud_service_providers_controller.rb
@@ -23,10 +23,41 @@ module Api
       action_result(false, e.to_s)
     end
 
+    def sync_collection(type, data)
+      provider_ids = data.fetch("provider_ids")
+      provider_ids = providers_ids.uniq.sort if provider_ids
+      raise BadRequest, "Must specify a list of provider ids via \"provider_ids\"" if provider_ids.empty?
+
+      invalid_provider_ids = find_provider_ids(type) - provider_ids
+      raise BadRequest, "Invalid Provider ids #{invalid_provider_ids.join(', ')} specified" if invalid_provider_ids.present?
+
+      desc = "Syncing Providers ids: #{provider_ids.join(', ')}"
+      task_id = Cfme::CloudServices::InventorySync.sync_queue(User.current_user.userid, providers)
+      action_result(true, desc, :task_id => task_id)
+    rescue => e
+      action_result(false, e.to_s)
+    end
+
+    def sync_all_collection(type, _data)
+      provider_ids = find_provider_ids(type)
+      raise NotFoundError, "There are no Providers to Sync" unless provider_ids.present?
+
+      desc = "Syncing All Providers"
+      task_id = Cfme::CloudServices::InventorySync.sync_queue(User.current_user.userid, provider_ids)
+      action_result(true, desc, :task_id => task_id)
+    rescue => e
+      action_result(false, e.to_s)
+    end
+
     private
 
     def provider_ident(provider)
       "Provider id:#{provider.id} name:'#{provider.name}'"
+    end
+
+    def find_provider_ids(type)
+      providers = collection_search(false, type, collection_class(type))
+      providers ? providers.ids.sort : []
     end
 
     def provider_types
