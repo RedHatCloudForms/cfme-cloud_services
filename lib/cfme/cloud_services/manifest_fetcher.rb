67413e4e0f282755b4197ac15c97504d3a258128
Alberto Bellotti <abellotti@users.noreply.github.com>
2019-08-01 11:34:21 -0400

    Merge pull request #31 from agrare/allow_verify_ssl_override
    Allow settings to override the verify_ssl param
    
    (cherry picked from commit 353b5509d04952249f4b2ce4421803b898fbc8a4)

--- lib/cfme/cloud_services/manifest_fetcher.rb
+++ lib/cfme/cloud_services/manifest_fetcher.rb
@@ -6,28 +6,31 @@ class Cfme::CloudServices::ManifestFetcher
     block_given? ? yield(manifest) : manifest
   end
 
-  private_class_method def self.certificate_config
+  private_class_method def self.manifest_configuration
+    ::Settings.cfme_cloud_services.manifest_configuration
+  end
+
+  private_class_method def self.certificate_options
     {:ssl_client_cert => OpenSSL::X509::Certificate.new(File.read("/etc/pki/consumer/cert.pem")),
-     :ssl_client_key  => OpenSSL::PKey::RSA.new(File.read("/etc/pki/consumer/key.pem")),
-     :verify_ssl      => OpenSSL::SSL::VERIFY_PEER}
+     :ssl_client_key  => OpenSSL::PKey::RSA.new(File.read("/etc/pki/consumer/key.pem"))}
+  end
+
+  private_class_method def self.ssl_options
+    {:verify_ssl => manifest_configuration.verify_ssl ? OpenSSL::SSL::VERIFY_PEER : OpenSSL::SSL::VERIFY_NONE}
   end
 
   cache_with_timeout(:raw_manifest) do
     begin
-      # TODO: Modeling
-      #   - Should we allow collection of non-model information, such as replication,
-      #     pg_* tables or filesystem level things?
-
-      manifest_config = ::Settings.cfme_cloud_services.manifest_configuration
-
       uri = URI::Generic.build(
-        :scheme => manifest_config.scheme,
-        :host   => manifest_config.host,
-        :port   => manifest_config.port,
-        :path   => File.join(manifest_config.path, Vmdb::Appliance.VERSION)
+        :scheme => manifest_configuration.scheme,
+        :host   => manifest_configuration.host,
+        :port   => manifest_configuration.port,
+        :path   => File.join(manifest_configuration.path, Vmdb::Appliance.VERSION)
       )
 
-      response = RestClient::Resource.new(uri.to_s, certificate_config)
+      options = certificate_options.merge(ssl_options)
+
+      response = RestClient::Resource.new(uri.to_s, options)
       response.get
     rescue StandardError => e
       _log.error("Error with obtaining manifest with schema: #{e.message}")
