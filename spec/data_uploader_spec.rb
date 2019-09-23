7ef08cc9bf5a46f588636ee5da9bb6640a212297
Adam Grare <agrare@redhat.com>
2019-09-23 09:48:22 -0400

    Merge pull request #48 from lpichler/log_failure_when_insights_clients_fail_with_exit_code0
    Log failure(by setting exit_code=1) when insights_client fails with exit_code=0
    
    (cherry picked from commit 3a8e0ded58453b5d1a21fccc4de7932f289e7ede)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1784533

--- spec/data_uploader_spec.rb
+++ spec/data_uploader_spec.rb
@@ -8,5 +8,25 @@ RSpec.describe Cfme::CloudServices::DataUploader do
 
       described_class.upload(path)
     end
+
+    let(:failure_output) do
+      'Uploading Insights data.' \
+      'Upload attempt 1 of 1 failed! Status code: 401' \
+      'All attempts to upload have failed!' \
+      'Please see /var/log/insights-client/insights-client.log for additional information'
+    end
+
+    let(:failure_command_result) do
+      AwesomeSpawn::CommandResult.new("", "", failure_output, 0)
+    end
+
+    it "returns false when insights-client returns failure output with exit code 0" do
+      path = "/tmp/cfme_upload.tar.gz"
+      expected_params = {:payload= => path, :content_type= => "application/vnd.redhat.topological-inventory.something+tgz"}
+
+      expect(AwesomeSpawn).to receive(:run).with("insights-client", :params => expected_params).and_return(failure_command_result)
+
+      expect(described_class.upload(path)).to be_falsey
+    end
   end
 end
